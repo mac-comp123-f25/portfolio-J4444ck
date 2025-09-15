f7cf537702962ab140bb29ea3aab56dc6239e2bb
J4444ck <qdong@macalester.edu>
2025-09-15 10:36:40 -0500

    ica

---
+++ src/ica/fun_basic/my_avg.py
@@ -0,0 +1,10 @@
+def ave3(x,y,z):
+    ave3=(x+y+z)/3
+    return ave3
+res = ave3(2, 3.0, 4.0)
+print(res)
+
+print(ave3(2,3,4))
+
+
+
---
+++ src/ica/fun_basic/square.py
@@ -0,0 +1,11 @@
+import turtle
+def turtle_square(sqTurt, side_len):
+    pass
+
+
+win = turtle.Screen()
+tt = turtle.Turtle()
+turtle_square(tt, 50)
+turtle_square(tt, 10)
+turtle_square(tt, 73)
+win.exitonclick()
